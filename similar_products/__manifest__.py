c382f38335fa83419ad6f2788a65649ccac8d81f
amcgeough <andrewmcgeough@gmail.com>
2021-10-12 23:22:32 +0100

    add support email

--- similar_products/__manifest__.py
+++ similar_products/__manifest__.py
@@ -6,10 +6,8 @@
     'author' : 'Andrew McGeough',
     'website' : 'wwww.mortimerapps.com',
 	'category': 'Products',
-	'summary':  """Display similar products via fuzzy search on product name""",
-	'description': """
-	Requires OCA addons base_search_fuzzy (provides Postgres fuzzy/trigram search)
-	""",
+	'summary':  'Display similar products via fuzzy search on product name',
+	'description': 'Requires OCA addons base_search_fuzzy (provides Postgres fuzzy/trigram search)',
 	'depends': ['product', 'base_search_fuzzy'],
 	'data': [
 		'views/product_template_views.xml',
@@ -20,5 +18,8 @@
 	'application' : True,
 	'auto_install' : False,
     'license': 'AGPL-3',
-	'image': 'images/similar-products.png'
+	'image': 'images/similar-products.png',
+	'support': 'info@mortimerapps.com',
+	# 'price': 100,
+	# 'currency': 'EUR'
 }
