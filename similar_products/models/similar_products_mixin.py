5ef54ee9eb622e3b7121e68dfd62571e33c10f4c
amcgeough <andrewmcgeough@gmail.com>
2022-03-29 14:57:26 +0100

    iinclude extra products in test

--- similar_products/models/similar_products_mixin.py
+++ similar_products/models/similar_products_mixin.py
@@ -3,11 +3,15 @@ from odoo import models, fields
 
 class SimilarProductsMixIn(models.AbstractModel):
     _name = 'similar.products.mixin'
+    _description = 'Resuable abstract mixin class for similar products'
 
     def similar(self):
         """
         View similar products using Name field via pg base_fuzzy addon
         """
+
+        # Clear previous results
+        self.env['similar.products'].search([(f"{self._name.replace('.', '_')}_id",'=',self.id)]).unlink()
 
         # Set the fuzzy match threshold using config setting value
         threshold = self.env['ir.config_parameter'].sudo().get_param('similar_products.similarity_threshold') or 0.3
@@ -17,16 +21,14 @@ class SimilarProductsMixIn(models.AbstractModel):
         # base_search_fuzzy postgres search addon (OCA) - %
         similar_products = self.env['product.template'].search([('name', '%', self.name)])
         
-        # Clear previous results and add new ones
+        # Add new results
         for product_template in similar_products:
             product_product = self.env['product.product'].search([('product_tmpl_id','=',product_template.id)], limit=1)
             
             if self._name == 'product.template' and self.id != product_template.id:
-                self.env['similar.products'].search([('product_template_id','=',self.id)]).unlink()
                 self.env['similar.products'].create({'product_template_id': self.id, 'similar_id': product_template.id})
 
             elif self._name == 'product.product' and self.id != product_product.id:
-                self.env['similar.products'].search([('product_product_id','=',self.id)]).unlink()
                 self.env['similar.products'].create({'product_product_id': self.id, 'similar_id': product_template.id})
 
             else:
