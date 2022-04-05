c170375f7b9ab357088fc9dfb517c87f696e532b
amcgeough <andrewmcgeough@gmail.com>
2022-04-05 15:09:05 +0100

    update manifest to mortimer

--- sales_order_name_unique/__manifest__.py
+++ sales_order_name_unique/__manifest__.py
@@ -3,8 +3,8 @@
 {
 	'name' : 'Sales Order Number/Name Unique',
 	'version': '15.0.1',
-    'author' : 'Andrew McGeough',
-    'website' : '',
+    'author' : 'Mortimer Apps',
+    'website' : 'https://www.mortimerapps.com',
 	'category': 'Products',
 	'summary':  'Sales Order Number/Name Unique',
 	'description': 'Sales Order Number/Name Unique',
