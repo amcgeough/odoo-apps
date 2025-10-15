ebec314673df9a7515fd38e4ee9176681a200edb
amcgeough <andrewmcgeough@gmail.com>
2025-10-15 13:21:57 +0000

    update author name

--- similar_products/__manifest__.py
+++ similar_products/__manifest__.py
@@ -3,8 +3,7 @@
 {
 	'name' : 'Similar Products',
 	'version': '15.0.2',
-    'author' : 'Mortimer Apps',
-    'website' : 'https://www.mortimerapps.com',
+    'author' : 'Andrew McG',
 	'category': 'Products',
 	'summary':  'Display similar products via fuzzy search on product name',
 	'description': 'Requires OCA addons base_search_fuzzy (provides Postgres fuzzy/trigram search)',
